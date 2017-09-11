99df20b2f8b530b03457b5e9dea1118743780bbc
kmsheng <kmsh3ng@gmail.com>
2017-09-12 02:28:52 +0800

    add file addWord.js

---
+++ src/models/addWord.js
@@ -0,0 +1,16 @@
+const db = require('./db');
+
+module.exports = function addWord({word, sentence, description, part}) {
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      const stmt = db.prepare('INSERT INTO Word (word, sentence, description, part) VALUES (?, ?, ?, ?)');
+      stmt.run(word, sentence, description, part, (err) => {
+        if (err) {
+          reject(err);
+        }
+      });
+      stmt.finalize();
+    });
+    resolve();
+  });
+}
