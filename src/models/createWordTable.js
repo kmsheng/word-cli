29bee64e5e842a0919ffa0590f20ee2a4a27eb50
kmsheng <kmsh3ng@gmail.com>
2017-09-12 02:28:37 +0800

    add file createWordTable.js

---
+++ src/models/createWordTable.js
@@ -0,0 +1,19 @@
+const db = require('./db');
+
+module.exports = function init() {
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      db.run(`
+        CREATE TABLE IF NOT EXISTS Word (word TEXT, part INTEGER DEFAULT 0 NOT NULL, sentence TEXT,
+        description TEXT, answerCorrect INTEGER DEFAULT 0 NOT NULL,
+        answerWrong INTEGER DEFAULT 0 NOT NULL);
+        CREATE UNIQUE INDEX \`word_word_unique\` ON \`Word\` (\`word\`, \`part\`)
+      `, (err) => {
+        if (err) {
+          reject(err);
+        }
+      });
+    });
+    resolve();
+  });
+}
