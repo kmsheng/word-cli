d711614dd70dda6ee39b0a7dafb0d9c0140b8755
kmsheng <kmsh3ng@gmail.com>
2017-09-12 23:30:06 +0800

    add function getRandomWord

---
+++ src/models/getRandomWord.js
@@ -0,0 +1,14 @@
+const db = require('./db');
+
+module.exports = function getRandomWord() {
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      db.all('SELECT rowid, * FROM Word ORDER BY RANDOM() LIMIT 1', (err, [row]) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(row);
+      });
+    });
+  });
+}
