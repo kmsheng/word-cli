282de1cd52ad7ce03b5b2da291c61f352c33db76
kmsheng <kmsh3ng@gmail.com>
2017-09-12 23:33:36 +0800

    test LIMIT OFFSET sql

--- src/models/listWords.js
+++ src/models/listWords.js
@@ -3,7 +3,7 @@ const db = require('./db');
 module.exports = function listWords() {
   return new Promise((resolve, reject) => {
     db.serialize(() => {
-      db.all('SELECT rowid, * FROM Word', (err, rows) => {
+      db.all(`SELECT rowid, * FROM Word LIMIT 10 OFFSET 1`, (err, rows) => {
         if (err) {
           return reject(err);
         }
