d20551d8a8bc80172b491216acfe7c425c3af0f7
kmsheng <kmsh3ng@gmail.com>
2017-09-12 23:30:27 +0800

    add function handleStudy

---
+++ src/controllers/handleStudy.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt');
+const getRandomWord = require('./../models/getRandomWord');
+
+module.exports = async function handleStudy() {
+
+  (async function askQuestion() {
+    const randomWord = await getRandomWord();
+    const sentence = randomWord.sentence.replace(/\[.+\]/, (matched) => '_ '.repeat(matched.length - 2).trim());
+    prompt.start();
+    prompt.get({properties: {name: {description: sentence}}}, (err, res) => {
+      if (err) {
+        return;
+      }
+      return askQuestion();
+    });
+  })();
+}
