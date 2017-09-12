35ae1e3f52a1842003d14e9689eadcb0e1626fd5
kmsheng <kmsh3ng@gmail.com>
2017-09-12 15:56:39 +0800

    add rowid to be selected

--- src/models/listWords.js
+++ src/models/listWords.js
@@ -3,7 +3,7 @@ const db = require('./db');
 module.exports = function listWords() {
   return new Promise((resolve, reject) => {
     db.serialize(() => {
-      db.all('SELECT * FROM Word', (err, rows) => {
+      db.all('SELECT rowid, * FROM Word', (err, rows) => {
         if (err) {
           return reject(err);
         }
