baba21e2e8427e6aa812596dd3e8dd1123e0800f
kmsheng <kmsh3ng@gmail.com>
2017-09-13 00:28:10 +0800

    rename answerWrong -> countAnswerCorrect

--- src/models/word/createWordTable.js
+++ src/models/word/createWordTable.js
@@ -5,8 +5,8 @@ module.exports = function createWordTable() {
     db.serialize(() => {
       db.run(`
         CREATE TABLE IF NOT EXISTS Word (word TEXT, part INTEGER DEFAULT 0 NOT NULL, sentence TEXT,
-        answerWrong INTEGER DEFAULT 0 NOT NULL);
         description TEXT, countAnswerCorrect INTEGER DEFAULT 0 NOT NULL,
+        countAnswerWrong INTEGER DEFAULT 0 NOT NULL);
       `, (err) => {
         if (err) {
           return reject(err);
