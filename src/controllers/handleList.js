7fca749c92e9b138414047fb65fa41b559ce24ff
kmsheng <kmsh3ng@gmail.com>
2017-09-13 00:35:56 +0800

    count word total

--- src/controllers/handleList.js
+++ src/controllers/handleList.js
@@ -1,8 +1,10 @@
 const listWords = require('./../models/word/listWords');
+const countWords = require('./../models/word/countWords');
 
 module.exports = async function handleList() {
 
   const words = await listWords();
+  const count = await countWords();
 
   if (words.length > 0) {
 
@@ -10,6 +12,7 @@ module.exports = async function handleList() {
     .join('\n');
 
     console.log(str);
+    console.log(`Total ${count}`);
   }
   else {
     console.log('You don\'t have any words yet.');
