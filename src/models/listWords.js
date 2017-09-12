ec577a911df1610ec1dbc271a693ecadd649535a
kmsheng <kmsh3ng@gmail.com>
2017-09-13 00:01:31 +0800

    add page and perPage

--- src/models/listWords.js
+++ src/models/listWords.js
@@ -1,9 +1,11 @@
 const db = require('./db');
 
-module.exports = function listWords() {
+module.exports = function listWords(...args) {
+  const page = args[0] || 1;
+  const perPage = args[1] || 10;
   return new Promise((resolve, reject) => {
     db.serialize(() => {
-      db.all(`SELECT rowid, * FROM Word LIMIT 10 OFFSET 1`, (err, rows) => {
+      db.all(`SELECT rowid, * FROM Word LIMIT ${perPage} OFFSET ${(page - 1) * perPage}`, (err, rows) => {
         if (err) {
           return reject(err);
         }
