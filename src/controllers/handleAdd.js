42ca0d7b2861f5f007c5d95b1d200499b4dea367
kmsheng <kmsh3ng@gmail.com>
2017-09-17 00:55:00 +0800

    allow no brackt case

--- src/controllers/handleAdd.js
+++ src/controllers/handleAdd.js
@@ -2,11 +2,13 @@ const addWord = require('./../models/word/addWord');
 
 module.exports = async function handleAdd(sentence, description) {
 
-  const [, word] = sentence.match(/\[(.+)\]/) || [];
+  let [, word] = sentence.match(/\[(.+)\]/) || [];
 
   if (! word) {
-    return console.log('Answer is not detected in the given sentence.\nPlease wrap the answer in brackets. For example, today is a [good] day.');
+    word = sentence;
+    sentence = `[${sentence}]`;
   }
+
   if (! description) {
     return console.log('Description of a word is required.');
   }
