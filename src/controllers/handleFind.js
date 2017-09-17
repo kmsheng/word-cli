ea5cf507ce82f6b1528b1740de3a66df4ef4cb56
kmsheng <kmsh3ng@gmail.com>
2017-09-17 16:24:19 +0800

    format find message

--- src/controllers/handleFind.js
+++ src/controllers/handleFind.js
@@ -2,5 +2,14 @@ const findWord = require('./../models/word/findWord');
 
 module.exports = async function handleFind(word) {
   const rows = await findWord(word)
-  console.log(rows);
+  if (0 === rows.length) {
+    return console.log(`No result for ${word}.`);
+  }
+
+  const str = rows.map(({rowid, word, countAnswerCorrect, countAnswerWrong, sentence, description}) => {
+    return `#${rowid} ${word} correct: ${countAnswerCorrect} wrong: ${countAnswerWrong} ${sentence} ${description}`
+  })
+  .join('\n');
+
+  console.log(str);
 }
