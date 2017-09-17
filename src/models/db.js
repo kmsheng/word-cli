29c67f43b5332e530106c2346c1220d7da63a34e
kmsheng <kmsh3ng@gmail.com>
2017-09-17 17:05:41 +0800

    persis db for upgrading

--- src/models/db.js
+++ src/models/db.js
@@ -1,4 +1,11 @@
+const mkdirp = require('mkdirp');
+const path = require('path');
+
+const dbPath = path.resolve(process.env.HOME, '.word-cli');
+
+mkdirp.sync(dbPath);
+
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(__dirname + '/db.sqlite3');
+const db = new sqlite3.Database(path.resolve(dbPath, 'db.sqlite3'));
 
 module.exports = db;
